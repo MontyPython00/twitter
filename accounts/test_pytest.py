0a0d7aef96d12df06a6c1421109d6785ecc8af97
montypython <montiback22@gmail.com>
2023-08-28 15:23:36 +0200

    accounts test

--- accounts/test_pytest.py
+++ accounts/test_pytest.py
@@ -1,8 +1,22 @@
 import pytest
 
+from django.contrib.auth.models import User
+from django.urls import reverse
 from accounts.forms import LoginForm, RegisterForm
 
 pytestmark = pytest.mark.django_db
 
 
 #Integration Tests 
+
+
+def test_create_account_client_success(client):
+    client_url = reverse('accounts:signup')
+    response = client.post(path=client_url, data={
+        "username":"test123123",
+        "email": "test@domain.pl",
+        "password1": "myPassword123",
+        "password2": "myPassword123"
+    })
+    assert response.status_code == 302
+    assert User.objects.all().count() == 1
